ba3c10e5030067cc4fc9c8c422bfc14a7beca2ef
RomanLyshchyk <romalyshik@gmail.com>
2021-06-01 18:15:33 +0300

    lesson-03
    Было выполнено задание на объединение массивов  с последующей сортировкой.

---
+++ SenlaTask/src/main/kotlin/Task1.java
@@ -0,0 +1,36 @@
+public class Task1 {
+    public static void main(String[] args) {
+        int[] list1 = new int[] { 2, 3, 5, 7, 11 };
+        int[] list2 = new int[] { 2, 4, 6, 8, 10, 12, 14 };
+        int[] result = merge(list1, list2);
+
+        for (int val : result) {
+            System.out.print(val+" ");
+        }
+    }
+
+
+    public static int[] merge(int[] arrayFirst, int[] arraySecond){
+        int[]sortedArray = new int[arrayFirst.length+arraySecond.length];
+        int count = 0;
+
+        for(int i = 0; i<arrayFirst.length; i++) {
+            sortedArray[i] = arrayFirst[i];
+            count++;
+        }
+        for(int j = 0;j<arraySecond.length;j++) {
+            sortedArray[count++] = arraySecond[j];
+        }
+
+        for (int i = 0; i < sortedArray.length - 1; i++) {
+            for (int j = sortedArray.length - 1; j > i; j--) {
+                if (sortedArray[j - 1] > sortedArray[j]) {
+                    int tmp = sortedArray[j - 1];
+                    sortedArray[j - 1] = sortedArray[j];
+                    sortedArray[j] = tmp;
+                }
+            }
+        }
+        return sortedArray;
+    }
+}
